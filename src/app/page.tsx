9486a208b1d594b805bbcf82bb86503a81fc6650
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-11-03 17:04:17 +0530

    layout changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import MaxWidthWrapper from "@/components/maxwidthwrapper";
+
 export default function Home() {
-	return <div>Landing Page</div>;
+  return (
+    <MaxWidthWrapper className="mb-12 mt-20 sm:mt-40 flex flex-col justify-center items-center text-center">
+      <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
+        <p className="text-sm font-semibold text-gray-700">Otto is now public!</p>
+      </div>
+    </MaxWidthWrapper>
+  );
 }
