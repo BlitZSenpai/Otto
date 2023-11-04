48be4d9d734a9c161dff1cf5328cd6611eba2448
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-11-04 16:38:33 +0530

    auth-callback

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,7 +1,15 @@
-import { router } from "./trpc";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { publicProcedure, router } from "./trpc";
+import { TRPCError } from "@trpc/server";
+
 export const appRouter = router({
-  // ...
+  authCallback: publicProcedure.query(async () => {
+    const { getUser } = getKindeServerSession();
+    const user = await getUser();
+
+    if (!user.id || !user.email) throw new TRPCError({ code: "UNAUTHORIZED" });
+    return { success: true };
+  }),
 });
-// Export type router type signature,
-// NOT the router itself.
+
 export type AppRouter = typeof appRouter;
