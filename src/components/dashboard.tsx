7ada14948fdf847035f211196ca1d656fc59f098
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-11-05 18:20:13 +0530

    add: message and delete button

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -2,10 +2,11 @@
 
 import { trpc } from "@/app/_trpc/client";
 import { UploadButton } from "./uploadbutton";
-import { Ghost, Plus } from "lucide-react";
+import { Ghost, MessageSquare, Plus, Trash } from "lucide-react";
 import Skeleton from "react-loading-skeleton";
 import Link from "next/link";
 import { format } from "date-fns";
+import { Button } from "./ui/button";
 
 export const Dashboard = () => {
   const { data: files, isLoading } = trpc.getUserFiles.useQuery();
@@ -26,7 +27,7 @@ export const Dashboard = () => {
                 className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow transition hover:shadow-lg">
                 <Link href={`/dashboard/${file.id}`} className="flex flex-col gap-2">
                   <div className="pt-6 px-6 flex w-full items-center justify-between space-x-6">
-                    <div className="h-10 w-10 flex-shrink-0 rounded-full bg-gradient-to-r fron-cyan-500 to-blue-500" />
+                    <div className="h-10 w-10 flex-shrink-0 rounded-full bg-gradient-to-r from-cyan-500 to-blue-500" />
                     <div className="flex-1 truncate">
                       <div className="flex items-center space-x-3">
                         <h3 className="truncate text-lg font-medium text-zinc-900">{file.name}</h3>
@@ -39,6 +40,13 @@ export const Dashboard = () => {
                     <Plus className="h-4 w-4" />
                     {format(new Date(file.createdAt), "mmm yyyy")}
                   </div>
+                  <div className="flex items-center gap-2">
+                    <MessageSquare className="h-4 w-4" />
+                    mock
+                  </div>
+                  <Button className="w-full" size="sm" variant="destructive">
+                    <Trash className="h-4 w-4" />
+                  </Button>
                 </div>
               </li>
             ))}
