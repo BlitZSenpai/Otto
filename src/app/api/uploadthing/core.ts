8405020cf3e9f4d22e156bc5b520cc56b2b5dd85
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-11-07 19:06:51 +0530

    add: uploadthing lib

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,3 +1,4 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 
 const f = createUploadthing();
@@ -5,7 +6,12 @@ const f = createUploadthing();
 export const ourFileRouter = {
   imageUploader: f({ image: { maxFileSize: "4MB" } })
     .middleware(async ({ req }) => {
-      return {};
+      const { getUser } = getKindeServerSession();
+      const user = await getUser();
+
+      if (!user || !user.id) throw new Error("Unauthorized");
+
+      return { userId: user.id };
     })
     .onUploadComplete(async ({ metadata, file }) => {}),
 } satisfies FileRouter;
