d5bfc2326d494fd3833626f03dfe5b9a0a3ab096
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-11-07 21:53:29 +0530

    add: pdf rendering through react-pdf

--- src/app/dashboard/[fileId]/page.tsx
+++ src/app/dashboard/[fileId]/page.tsx
@@ -31,7 +31,7 @@ const FileIdPage = async ({ params }: FileIdProps) => {
       <div className="mx-auto w-full max-w-8xl grow lg:flex xl:px-2">
         <div className="flex-1 xl:flex">
           <div className="px-4 py-6 sm:px-6 lg:px-8 xl:flex-1 xl:pl-6">
-            <PdfRenderer />
+            <PdfRenderer url={file.url} />
           </div>
         </div>
         <div className="shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-1 lg:border-t-0">
--- src/components/pdfrenderer.tsx
+++ src/components/pdfrenderer.tsx
@@ -1,8 +1,23 @@
-export const PdfRenderer = () => {
+import { Document, Page } from "react-pdf";
+import "react-pdf/dist/Page/TextLayer.css";
+import "react-pdf/dist/Page/AnnotationLayer.css";
+
+interface PdfRendererProps {
+  url: string;
+}
+
+export const PdfRenderer = ({ url }: PdfRendererProps) => {
   return (
     <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
       <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between">
         <div className="flex items-center gap-1.5">Top layer</div>
+      </div>
+      <div className="flex-1 w-full max-h-screen">
+        <div>
+          <Document file={url} className="max-h-full">
+            <Page pageNumber={1} />
+          </Document>
+        </div>
       </div>
     </div>
   );
