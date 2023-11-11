ec217231b4fd20fc30d6f1f863b92346149ae090
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-11-11 22:38:00 +0530

    fix: form fix

--- src/components/chat/chatinput.tsx
+++ src/components/chat/chatinput.tsx
@@ -14,7 +14,7 @@ export const ChatInput = ({ isDisabed }: ChatInputProps) => {
 
   return (
     <div className="absolute bottom-0 left-0 w-full">
-      <form className="mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
+      <div className="mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
         <div className="relative flex h-full flex-1 items-stretch md:flex-col">
           <div className="relative">
             <Textarea
@@ -38,7 +38,6 @@ export const ChatInput = ({ isDisabed }: ChatInputProps) => {
             />
             <Button
               disabled={isLoading || isDisabed}
-              type="submit"
               onSubmit={() => {
                 addMessage();
                 textareaRef.current?.focus();
@@ -49,7 +48,7 @@ export const ChatInput = ({ isDisabed }: ChatInputProps) => {
             </Button>
           </div>
         </div>
-      </form>
+      </div>
     </div>
   );
 };
