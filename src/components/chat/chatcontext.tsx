ea6e641f0b7df411f85ed5dce6fd30e4a8c9b28c
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-11-12 18:46:13 +0530

    add: optimostic message rendering

--- src/components/chat/chatcontext.tsx
+++ src/components/chat/chatcontext.tsx
@@ -1,6 +1,8 @@
-import { ReactNode, createContext, useState } from "react";
+import { ReactNode, createContext, useRef, useState } from "react";
 import { useToast } from "../ui/use-toast";
 import { useMutation } from "@tanstack/react-query";
+import { trpc } from "@/app/_trpc/client";
+import { INFINITE_QUERY_LIMIT } from "@/config/infinite-query";
 
 type StreamResponse = {
   addMessage: () => void;
@@ -25,6 +27,8 @@ export const ChatContextProvider = ({ fileId, children }: Props) => {
   const [message, setMessage] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const { toast } = useToast();
+  const utils = trpc.useUtils();
+  const backupMessage = useRef("");
 
   const { mutate: sendMessage } = useMutation({
     mutationFn: async ({ message }: { message: string }) => {
@@ -39,6 +43,46 @@ export const ChatContextProvider = ({ fileId, children }: Props) => {
         throw new Error("Failed to send message");
       }
       return response.body;
+    },
+    onMutate: async ({ message }) => {
+      backupMessage.current = message;
+      setMessage("");
+      //step 1
+      await utils.getFileMessages.cancel();
+      //step 2
+      const previousMessages = utils.getFileMessages.getInfiniteData();
+      //step 3
+      utils.getFileMessages.setInfiniteData({ fileId, limit: INFINITE_QUERY_LIMIT }, (old) => {
+        if (!old) {
+          return {
+            pages: [],
+            pageParams: [],
+          };
+        }
+        let newPages = [...old.pages];
+        let latestPage = newPages[0];
+
+        latestPage.messages = [
+          {
+            createdAt: new Date().toISOString(),
+            id: crypto.randomUUID(), // basically moving the old messages to the top
+            text: message, //whereever we send a new message
+            isUserMessage: true,
+          },
+          ...latestPage.messages,
+        ];
+        newPages[0] = latestPage; //changing and inserting the data into newpages
+
+        return {
+          ...old,
+          pages: newPages,
+        };
+      });
+      setIsLoading(true);
+
+      return {
+        previousMessages: previousMessages?.pages.flatMap((page) => page.messages) ?? [],
+      };
     },
   });
 
