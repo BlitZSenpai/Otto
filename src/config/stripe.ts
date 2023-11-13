5beaf14fb406e3089912e493b4d484716ea211a1
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-11-13 16:19:13 +0530

    add: stripe config

---
+++ src/config/stripe.ts
@@ -0,0 +1,28 @@
+export const PLANS = [
+  {
+    name: "Free",
+    slug: "free",
+    quote: 20,
+    pagesPerPdf: 15,
+    price: {
+      amount: 0,
+      priceIds: {
+        test: "",
+        production: "",
+      },
+    },
+  },
+  {
+    name: "Free",
+    slug: "free",
+    quote: 50,
+    pagesPerPdf: 35,
+    price: {
+      amount: 450,
+      priceIds: {
+        test: "price_1OBiLYSGz5UdrO0L9BN9t1f4",
+        production: "",
+      },
+    },
+  },
+];
