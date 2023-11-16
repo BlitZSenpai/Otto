099aaa0f8a018d27ce1afaac72334fc8c26c1210
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-11-16 13:35:24 +0530

    fix: next config image

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "assets.vercel.com",
+        port: "",
+        pathname: "/image/upload/**",
+      },
+    ],
+    domains: ["lh3.googleusercontent.com", "avatars.githubusercontent.com"],
+  },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.resolve.alias.canvas = false;
     config.resolve.alias.encoding = false;
