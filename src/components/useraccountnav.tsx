54d5ea413942b3f2265bf7879b23b1bec0cf7e5e
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-11-16 13:55:56 +0530

    add: navbar changes

--- src/components/useraccountnav.tsx
+++ src/components/useraccountnav.tsx
@@ -1,9 +1,18 @@
 import { getUserSubscriptionPlan } from "@/lib/stripe";
-import { DropdownMenu, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 import { Button } from "./ui/button";
 import { Avatar, AvatarFallback } from "./ui/avatar";
 import Image from "next/image";
 import { Icons } from "./icons";
+import Link from "next/link";
+import { Gem } from "lucide-react";
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/dist/components";
 
 interface UserAccountNavProps {
   name: string;
@@ -31,6 +40,33 @@ export const UserAccountNav = async ({ name, email, imageUrl }: UserAccountNavPr
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
+      <DropdownMenuContent className="bg-white" align="end">
+        <div className="flex items-center justify-start gap-2 p-2">
+          <div className="flex flex-col space-y-0.5 leading-none">
+            {name && <p className="font-medium text-xs text-black">{name}</p>}
+            {email && <p className="w-[200px] truncate text-xs text-zinc-700">{email}</p>}
+          </div>
+        </div>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem asChild className="cursor-pointer">
+          <Link href="/dashboard">Dashboard</Link>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem asChild className="cursor-pointer">
+          {subscriptionPlan?.isSubscribed ? (
+            <Link href="/dashboard/billing">Manage Subscription</Link>
+          ) : (
+            <Link href="/pricing">
+              Upgrade
+              <Gem className="h-4 w-4 ml-1" />
+            </Link>
+          )}
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem asChild className="cursor-pointer mb-1">
+          <LogoutLink>Log out</LogoutLink>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
     </DropdownMenu>
   );
 };
