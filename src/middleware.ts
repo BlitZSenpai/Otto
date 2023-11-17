4ed22124bec6378845d0b55a28119cf839f852cc
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-11-17 23:59:07 +0530

    middle ware update

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@kinde-oss/kinde-auth-nextjs/server";
 
 export const config = {
-  matcher: ["/:path", "/auth-callback"],
+  matcher: ["/"],
 };
 
 export default authMiddleware;
